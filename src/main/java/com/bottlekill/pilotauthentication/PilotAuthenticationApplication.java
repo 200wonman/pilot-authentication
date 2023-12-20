96a95f58603e978f57c9266736d5660e3d804308
KIM HYUN GI <rlagusrl928@gmail.com>
2023-12-20 21:25:42 +0900

    set up project

---
+++ src/main/java/com/bottlekill/pilotauthentication/PilotAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.bottlekill.pilotauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PilotAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bottlekill/pilotauthentication/PilotAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bottlekill.pilotauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
